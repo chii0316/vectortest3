e4c2955754174d26ba6b5c0ea1e5c0e6c371a9a3
IDEHARA Norimichi <idehara@tama.ac.jp>
2021-11-30 15:44:24 +0900

    first commit

---
+++ Assets/kadai2/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject tama;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Space) )
+        {
+            Vector3 p = gameObject.transform.position;
+            GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+            Vector3 e = enemy.transform.position;
+
+            Vector3 v = e-p;
+            tama.GetComponent<TamaMover>().velocity = v;
+        }
+
+        if( Input.GetKeyDown(KeyCode.R) )
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/kadai2/Scripts/TamaMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TamaMover : MonoBehaviour
+{
+    public Vector3 velocity;
+    Vector3 orginalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector3.zero;
+        orginalPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+        if( Mathf.Abs(transform.position.x) > 20.0f || Mathf.Abs(transform.position.z) > 20.0f )
+        {
+            transform.position = orginalPosition;
+            velocity = Vector3.zero;
+        }
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        Debug.Log("Hit!");
+        if( c.gameObject.CompareTag("Enemy") )
+        {
+            c.gameObject.SetActive(false);
+        }
+    }
+}
